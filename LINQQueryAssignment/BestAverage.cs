1829c2c99341559fd97eff801f8d18c3bf0ba4c0
seanob30 <seanobrien2014@gmail.com>
2017-01-09 12:28:53 -0600

    simplified frequency function and minor work on average

--- LINQQueryAssignment/BestAverage.cs
+++ LINQQueryAssignment/BestAverage.cs
@@ -10,15 +10,27 @@ namespace LINQQueryAssignment
     {
         public double TotalGradesAverage(List<string> grades)
         {
-            int studentNumber = grades.Count;
-            string [] soloGrades = string.Join<string>(",", grades).Split(',');
+            string[] soloGrades = string.Join<string>(",", grades).Split(',');
             var result = Array.ConvertAll(soloGrades, int.Parse).OrderByDescending(x => x).ToList();
-            for(int i = 0; i < studentNumber; i++)
+            foreach (var element in grades)
             {
                 result.Remove(result.Min());
             }
             double classAverage = result.Average();
             return classAverage;
+
+
         }
+            ////experimentation*****
+            //var temporaryString = string.Join<string>(",", grades);
+            //var result = from c in temporaryString
+            //             group c by c;
+            //foreach (var item in grades)
+            //{
+            //    result.Reverse().Skip(1).Reverse();
+            //}
+            //double classAverage = result.ToList().Average();
+            //return classAverage;
+        
     }
 }
--- LINQQueryAssignment/CharacterFrequency.cs
+++ LINQQueryAssignment/CharacterFrequency.cs
@@ -10,11 +10,12 @@ namespace LINQQueryAssignment
     {
         public void Order(string input)
         {
-            char[] dummy = input.ToArray();
-            Array.Sort(dummy);
-            string temporaryString = new string(dummy);
-            var wordCount = temporaryString.GroupBy(x => x).Select(x => new { Character = x.Key, Count = x.Count() });
-            foreach (var item in wordCount) Console.WriteLine("Alphabet :" + item.Character + ", Count :" + item.Count);
+            var result = from c in input.ToLower()
+                         group c by c;
+            foreach (var item in result)
+            {
+                Console.WriteLine("Alphabet :" + item.Key + ", Count :" + item.Count());
+            }
         }
     }
 }
